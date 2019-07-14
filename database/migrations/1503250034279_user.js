a357bf2c7a29fdf1a3b2443a9b67a5d97dee9257
William Pereira <williamcpereira89@gmail.com>
2019-07-14 14:59:43 -0300

    iniciando aula 14

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -4,19 +4,20 @@
 const Schema = use('Schema')
 
 class UserSchema extends Schema {
-  up () {
-    this.create('users', (table) => {
-      table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
-      table.timestamps()
-    })
-  }
+	up () {
+		this.create('users', (table) => {
+			table.increments()
+			table.string('name', 80)
+			table.string('email', 254).notNullable().unique()
+			table.string('password', 60).notNullable()
+			table.integer('image_id').unsigned()
+			table.timestamps()
+		})
+	}
 
-  down () {
-    this.drop('users')
-  }
+	down () {
+		this.drop('users')
+	}
 }
 
 module.exports = UserSchema
