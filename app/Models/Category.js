2981eb13a983a25622441706cdaf94efc4e1215d
William Pereira <williamcpereira89@gmail.com>
2020-01-05 20:51:18 -0300

    Categoriy model

--- app/Models/Category.js
+++ app/Models/Category.js
@@ -4,6 +4,21 @@
 const Model = use('Model')
 
 class Category extends Model {
+
+	/**
+	 * Relacionamento entre Categoria e Imagem de destaque
+	 */
+	image() {
+		return this.belongsTo('App/Models/Image')
+	}
+
+	/**
+	 * Relacionamento entre Categoria e Produtos
+	 */
+	products() {
+		return this.belongsToMany('App/Models/Product')
+	}
+
 }
 
 module.exports = Category
