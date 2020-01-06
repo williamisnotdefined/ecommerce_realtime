96248f7381d2f63c111f8051befc6107669cd013
William Pereira <williamcpereira89@gmail.com>
2020-01-06 01:34:35 -0300

    Coupon + Discount - andamento

--- app/Models/Coupon.js
+++ app/Models/Coupon.js
@@ -4,6 +4,23 @@
 const Model = use('Model')
 
 class Coupon extends Model {
+
+	static get dates() {
+		return ['created_at', 'updated_at', 'valid_from', 'valid_until']
+	}
+
+	users() {
+		return this.belongsToMany('App/Models/User')
+	}
+
+	products() {
+		return this.belongsToMany('App/Models/Product')
+	}
+
+	orders() {
+		return this.belongsToMany('App/Models/Order')
+	}
+
 }
 
 module.exports = Coupon
---
+++ app/Models/Discount.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Discount extends Model {
+
+	static get table() {
+		return 'coupon_order'
+	}
+
+	order() {
+		return this.belongsTo(
+			'App/Models/Order',
+			'order_id', // nome da chave na coupon_order
+			'id' // nome da chave na tabela coupon
+		)
+	}
+
+	coupon() {
+		return this.belongsTo('App/Models/Coupon', 'coupon_id', 'id')
+	}
+
+}
+
+module.exports = Discount
