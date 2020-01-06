ff46e5a1b87ffb0d6bfa2f88defd432f908d9dd0
William Pereira <williamcpereira89@gmail.com>
2020-01-06 03:24:05 -0300

    OrderHook

--- app/Models/Order.js
+++ app/Models/Order.js
@@ -5,6 +5,13 @@ const Model = use('Model')
 
 class Order extends Model {
 
+	static boot() {
+		super.boot()
+
+		this.addHook('afterFind', 'OrderHook.updateValues')
+		this.addHook('afterPaginate', 'OrderHook.updateCollectionValues')
+	}
+
 	items() {
 		return this.hasMany('App/Models/Item')
 	}
