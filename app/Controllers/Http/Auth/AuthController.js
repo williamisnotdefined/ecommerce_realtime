60fa8b6929dda75dacd831ef748cd9d5b73bf68d
William Pereira <williamcpereira89@gmail.com>
2020-01-11 17:50:06 -0300

    Adicionando controller e rotas auth

---
+++ app/Controllers/Http/Auth/AuthController.js
@@ -0,0 +1,35 @@
+'use strict'
+
+class AuthController {
+
+	async register({ req, res }) {
+		
+	}
+
+	async login({ req, res, auth }) {
+
+	}
+
+	async refresh({ req, res, auth }) {
+
+	}
+
+	async logout({ req, res, auth }) {
+
+	}
+
+	async forgot({ req, res }) {
+
+	}
+
+	async remember({ req, res }) {
+
+	}
+
+	async reset({ req, res }) {
+
+	}
+
+}
+
+module.exports = AuthController
---
+++ start/routes/auth.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.group(() => {
+	Route.post('/register', 'AuthController.register').as('auth.register')
+
+	Route.post('/login', 'AuthController.login').as('auth.login')
+	Route.post('/logout', 'AuthController.logout').as('auth.logout')
+
+	Route.post('/refresh', 'AuthController.refresh').as('auth.refresh')
+
+}).prefix('v1/auth')
+.namespace('Auth')
