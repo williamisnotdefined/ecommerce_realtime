4b0ad24078595edef33c7928ff784781b0da7842
William Pereira <williamcpereira89@gmail.com>
2020-01-11 18:40:36 -0300

    Coupon + Image + Order + User controller

---
+++ app/Controllers/Http/Admin/CouponController.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+* Resourceful controller for interacting with coupons
+*/
+class CouponController {
+
+
+    async index ({ request, response, view }) {
+    }
+
+
+    async store ({ request, response }) {
+    }
+
+
+    async show ({ params, request, response, view }) {
+    }
+
+
+    async update ({ params, request, response }) {
+    }
+
+
+    async destroy ({ params, request, response }) {
+    }
+
+
+}
+
+module.exports = CouponController
--- start/routes/admin.js
+++ start/routes/admin.js
@@ -7,5 +7,16 @@ Route.group(() => {
 
 	Route.resource('categories', 'CategoryController').apiOnly()
 
+	Route.resource('products', 'ProductController').apiOnly()
+
+	Route.resource('coupons', 'CouponController').apiOnly()
+
+	Route.resource('order', 'OrderController').apiOnly()
+
+	Route.resource('images', 'ImageController').apiOnly()
+
+	Route.resource('user', 'UserController').apiOnly()
+
+
 }).prefix('v1/admin')
 .namespace('Admin')
