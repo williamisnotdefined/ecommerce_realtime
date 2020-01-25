ad2a9c1174058bdfb922a3523787d9d1151ce9c5
William Pereira <williamcpereira89@gmail.com>
2020-01-25 02:59:30 -0300

    Iniciando validators

---
+++ app/Validators/Auth/Auth.js
@@ -0,0 +1,11 @@
+'use strict'
+
+class AuthAuth {
+  get rules () {
+    return {
+      // validation rules
+    }
+  }
+}
+
+module.exports = AuthAuth
--- start/routes/auth.js
+++ start/routes/auth.js
@@ -4,16 +4,16 @@
 const Route = use('Route')
 
 Route.group(() => {
-	Route.post('register', 'AuthController.register').as('auth.register')
+	Route.post('register', 'AuthController.register').as('auth.register').middleware(['guest'])
 
-	Route.post('login', 'AuthController.login').as('auth.login')
-	Route.post('logout', 'AuthController.logout').as('auth.logout')
+	Route.post('login', 'AuthController.login').as('auth.login').middleware(['guest'])
+	Route.post('logout', 'AuthController.logout').as('auth.logout').middleware(['auth'])
 
-	Route.post('refresh', 'AuthController.refresh').as('auth.refresh')
+	Route.post('refresh', 'AuthController.refresh').as('auth.refresh').middleware(['guest'])
 
-	Route.post('reset-password', 'AuthController.forgot').as('auth.forgot')
-	Route.get('reset-password', 'AuthController.remember').as('auth.remember')
-	Route.put('reset-password', 'AuthController.reset').as('auth.reset')
+	Route.post('reset-password', 'AuthController.forgot').as('auth.forgot').middleware(['guest'])
+	Route.get('reset-password', 'AuthController.remember').as('auth.remember').middleware(['guest'])
+	Route.put('reset-password', 'AuthController.reset').as('auth.reset').middleware(['guest'])
 
 }).prefix('v1/auth')
 .namespace('Auth')
