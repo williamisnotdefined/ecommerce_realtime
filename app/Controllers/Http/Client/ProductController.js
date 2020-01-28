f43b0fb2a550fc1cca47a208542d60845c316c62
William Pereira <williamcpereira89@gmail.com>
2020-01-28 20:43:07 -0300

    ProductController

--- app/Controllers/Http/Client/ProductController.js
+++ app/Controllers/Http/Client/ProductController.js
@@ -4,20 +4,38 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Product = use('App/Models/Product')
+const ProductTransformer = use('App/Transformers/Admin/ProductTransformer')
+
 /**
-* Resourceful controller for interacting with products
-*/
+ * Resourceful controller for interacting with products
+ */
 class ProductController {
+	async index({ request, response, transform, pagination }) {
+		const title = request.input('title')
+		const productQuery = Product.query()
 
+		if (title) {
+			productQuery.where('name', 'LIKE', `%${title}%`)
+		}
 
-    async index ({ request, response, view }) {
-    }
+		let products = await productQuery.paginate(
+			pagination.page,
+			pagination.limit
+		)
+		products = await transform.paginate(products, ProductTransformer)
 
+		return response.send(products)
+	}
 
-    async show ({ params, request, response, view }) {
-    }
+	async show({ params: { id }, request, response, transform }) {
+		let product = Product.findOrFail(id)
+		product = transform.paginate(product, ProductTransformer)
 
-
+		return response.send({
+			product
+		})
+	}
 }
 
 module.exports = ProductController
